060dd6b642940c30ede399938f8a5d7a094fb7e8
fishtoyo <yu-tingcheng@Fishs-MBPr.local>
2015-02-17 17:05:44 +0800

    調整資料夾結構、新增Photos List效果

---
+++ assets/js/common.js
@@ -0,0 +1,10 @@
+﻿jQuery(document).ready(function(e) {
+ 
+  // 下拉選單
+  if (jQuery('.selectBox')[0]) {
+    jQuery('.selectBox select').animate({ opacity: 0 },0).change(function(){
+      jQuery(this).parent().find('label').html(jQuery('option:selected', this).attr('title'));
+    });
+  }
+  
+});
